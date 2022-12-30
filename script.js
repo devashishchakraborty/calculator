716296da32d1839294dc549e1092244970490979
Devashish Chakraborty <devashishchakra@gmail.com>
2022-12-30 23:23:36 +0530

    Added operate function to execute calculation functions based on operator

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ function add(num1, num2){
     return num1 + num2;
 }
 
-function subtract(){
+function subtract(num1, num2){
     return num1 - num2;
 }
 
-function multiply(){
+function multiply(num1, num2){
     return num1 * num2;
 }
 
-function divide(){
+function divide(num1, num2){
     return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    switch (operator){
+        case "+" : add(num1, num2);
+        break;
+
+        case "-" : subtract(num1, num2);
+        break;
+
+        case "*" : multiply(num1, num2);
+        break;
+
+        case "/" : divide(num1, num2);
+        break;
+    }
 }
