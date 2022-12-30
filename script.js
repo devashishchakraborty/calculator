4514c7633f3c0f45c1ff869aadf189db8244c72b
Devashish Chakraborty <devashishchakra@gmail.com>
2022-12-30 23:31:16 +0530

    Returned the functions coz i forgot before lol

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ function divide(num1, num2){
 
 function operate(operator, num1, num2){
     switch (operator){
-        case "+" : add(num1, num2);
+        case "+" : return add(num1, num2);
         break;
 
-        case "-" : subtract(num1, num2);
+        case "-" : return subtract(num1, num2);
         break;
 
-        case "*" : multiply(num1, num2);
+        case "*" : return multiply(num1, num2);
         break;
 
-        case "/" : divide(num1, num2);
+        case "/" : return divide(num1, num2);
         break;
     }
 }
