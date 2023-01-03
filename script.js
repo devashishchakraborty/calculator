ce5a10e2c146b35876354eb819e753962816f410
Devashish Chakraborty <devashishchakra@gmail.com>
2023-01-04 00:46:12 +0530

    Rewritten the entire code base again to follow according to theodinproject instructions. Now the program runs better than before

--- script.js
+++ script.js
@@ -25,57 +25,55 @@ function operate(operator, num1, num2) {
         case "-": return subtract(num1, num2);
             break;
 
-        case "*": return multiply(num1, num2);
+        case "×": return multiply(num1, num2);
             break;
 
-        case "/": return divide(num1, num2);
+        case "÷": return divide(num1, num2);
             break;
     }
 }
 
-const operationArea = document.querySelector('#operation');
-const solutionArea = document.querySelector("#solution");
-const buttons = document.querySelectorAll('button')
+const operationArea = document.querySelector('.display-area');
+const buttons = document.querySelectorAll('.btn')
 
 let firstNumber = "";
 let secondNumber = "";
 let displayValue = "";
-let operator = "";
+let operation = "";
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function (event) {
-        if (!displayValue.includes("=") && secondNumber.length > 0 && button.getAttribute('id') === 'equals') {
-            solutionArea.textContent = operate(operator[0], parseFloat(firstNumber), parseFloat(secondNumber));
+
+        if (event.target.getAttribute("button-type") === "number") {
+            displayValue += event.target.getAttribute('value');
         }
-        if (displayValue.includes("=")) {
-            displayValue = firstNumber
-        }
-        displayValue += button.textContent;
-        operationArea.textContent = displayValue;
-        if (button.getAttribute('class') === 'number') {
-            const digit = button.getAttribute('number');
-            if (operator.length === 0) {
-                firstNumber += digit;
-            } else if (operator.length === 1) {
-                secondNumber += digit;
+
+        if (event.target.getAttribute("button-type") === "operator") {
+            if (+displayValue.slice(-1)) {
+                if (event.target.getAttribute("value") === "=") {
+                    secondNumber = displayValue.split(operation).filter(Boolean).slice(-1);
+                    displayValue = Math.round(operate(operation, parseFloat(firstNumber), parseFloat(secondNumber)) * 100) / 100;
+                } else {
+                    if (operation.length === 1) {
+                        secondNumber = displayValue.split(operation).filter(Boolean).slice(-1);
+                        displayValue = Math.round(operate(operation, parseFloat(firstNumber), parseFloat(secondNumber)) * 100) / 100;
+                        firstNumber = displayValue;
+                        displayValue += event.target.getAttribute("value");
+                    } else {
+                        firstNumber = displayValue;
+                        displayValue += event.target.getAttribute("value");
+                    }
+                    operation = event.target.getAttribute("value");
+                }
             }
         }
-        if (button.getAttribute('class') === 'operator') {
-            operator += button.getAttribute('operator');
-            if (operator.length === 2) {
-                firstNumber = operate(operator[0], parseFloat(firstNumber), parseFloat(secondNumber));
-                operator = operator.slice(1);
-                solutionArea.textContent = firstNumber;
-                secondNumber = "";
-            }
-        }
-        if (button.getAttribute('class') === 'clear') {
-            operationArea.textContent = "";
-            solutionArea.textContent = "0";
+
+        if (event.target.getAttribute("button-type") === "clear") {
             displayValue = "";
             firstNumber = "";
             secondNumber = "";
-            operator = "";
+            operation = "";
         }
+        operationArea.textContent = displayValue;
     })
 });
