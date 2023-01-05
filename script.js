cf21868518b780f5b528a6ffc51f290774faa447
Devashish Chakraborty <devashishchakra@gmail.com>
2023-01-05 15:29:29 +0530

    Added Exponent Functionality in the calculator

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function exponent(num1, num2){
+    return num1 ** num2;
+}
+
 function operate(operator, num1, num2) {
     switch (operator) {
         case "+": return add(num1, num2);
@@ -29,6 +33,9 @@ function operate(operator, num1, num2) {
             break;
 
         case "÷": return divide(num1, num2);
+            break;
+        
+        case "^": return exponent(num1, num2);
             break;
     }
 }
